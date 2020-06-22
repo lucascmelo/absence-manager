fa99c7d666ce52e6a544f14447f81cbd87187a52
Lucas Cavalcanti <lucas@mercadons.com.br>
2020-06-22 16:09:30 -0300

    - Custom Header - Import Logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Logo from '../assets/images/logo_crewmeister_white.svg';
 
 const Header: React.FC = () => (
   <header>
-    <h1 className='logo'><img src="https://d33wubrfki0l68.cloudfront.net/f140ee3478696725fd905f311242bfc48579b2f5/dcaca/images/logo_crewmeister_white.svg" alt="Crewmeister logo"/></h1>
-    <nav>
-      <Link to="/">Dashboard</Link>
-      <Link to="/report">Report</Link>
+    <h1 className='logo'><img src={Logo} alt="Crewmeister logo"/></h1>
+    <nav id='nav-main'>
+      <a href="#" className="icon-live"></a>
+      <a href="#" className="icon-time-tracking"></a>
+      <a href="#" className="icon-time-tracking-report"></a>
+      <a href="#" className="icon-shift-planner"></a>
+      <Link to="/report" className="icon-absence-management active"></Link>
+      <a href="#" className="icon-settings"></a>
+      <a href="#" className="icon-chat"></a>
+    </nav>
+    <nav className='user-actions'>
+      <a href="#" className="icon-alarm"></a>
+      <a href="#" className="icon-my-profile"></a>
     </nav>
   </header>
 )
