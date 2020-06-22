fabe714d7c9fd95e2af84588818c93269286dbf5
Lucas Cavalcanti <lucas@mercadons.com.br>
2020-06-22 20:25:25 -0300

    - Add module MomentJS and format date on table - Layout Filter - Improving CSS and HTML format

--- client/src/pages/Report/index.tsx
+++ client/src/pages/Report/index.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import Header from '../../components/Header';
 import Loader from '../../components/Loader';
+import Moment from 'react-moment';
+import "./style.css";
 
-import './style.css';
 interface Absence {
   id: number;
   employee: string;
@@ -38,25 +39,39 @@ const Report: React.FC = () => {
         <form action="">
           <fieldset>
             <legend>Filter</legend>
-            <div>
-              <label htmlFor="">Name employee</label>
-              <input type="text"/>
-            </div>
-            <div>
-              <label htmlFor="">Absence Types</label>
-              <input type="text"/>
-            </div>
-            <div>
-              <label htmlFor="">Status</label>
-              <input type="text"/>
-            </div>
-            <div>
-              <label htmlFor="">Start Date</label>
-              <input type="text"/>
-            </div>
-            <div>
-              <label htmlFor="">End Date</label>
-              <input type="text"/>
+            <div className="fields">
+              <div>
+                <label htmlFor="">Employee</label>
+                <input type="text"/>
+              </div>
+              <div>
+                <label htmlFor="">Absence Types</label>
+                <select name="status">
+                  <option defaultValue="all">All</option>
+                  <option value="vacation">Vacation</option>
+                  <option value="sickness">Sickness</option>
+                  <option value="compensatory" disabled>Compensatory time off</option>
+                  <option value="education" disabled>Education/Training</option>
+                  <option value="other" disabled>Other absence</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="">Status</label>
+                <select name="status">
+                  <option defaultValue="all">All</option>
+                  <option value="approved">Approved</option>
+                  <option value="rejected">Rejected</option>
+                  <option value="pending">Pending</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="">Start Date</label>
+                <input type="date"/>
+              </div>
+              <div>
+                <label htmlFor="">End Date</label>
+                <input type="date"/>
+              </div>
             </div>
           </fieldset>
         </form>
@@ -64,10 +79,9 @@ const Report: React.FC = () => {
           <thead>
             <tr>
               <th>Name</th>
-              <th>Type</th>
-              <th>Start Date</th>
-              <th>End Date</th>
-              <th>Download IC</th>
+              <th>Absence Types</th>
+              <th>Period</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
